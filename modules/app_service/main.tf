82e85eec8eedef8519cbe88b2bc5d5435cc6043a
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-06 01:18:02 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -7,41 +7,41 @@ resource "azurerm_service_plan" "strapi" {
   sku_name            = var.plan_sku
 }
 
+# Create the Linux Web App for Strapi
 resource "azurerm_linux_web_app" "strapi" {
   name                = "${var.clinic_name}-cms"
   resource_group_name = var.resource_group_name
   location            = var.location
   service_plan_id     = azurerm_service_plan.strapi.id
 
-  site_config {
-    #linux_fx_version = "NODE|18-lts"
-  }
+  # The runtime stack is now set automatically by Azure or configured via GitHub Actions
+  site_config {}
 
   app_settings = {
-    NODE_ENV                = "production"
-    STRAPI_ADMIN_EMAIL      = var.admin_email
-    STRAPI_ADMIN_PASSWORD   = var.admin_password
-    DATABASE_URL            = var.db_connection_string
-    LINKED_STOREFRONT_URL   = var.linked_storefront_url
-    BACKEND_URL             = var.backend_url
-    BRAND_PRIMARY_COLOR     = var.brand_primary_color
-    BRAND_SECONDARY_COLOR   = var.brand_secondary_color
-    BRAND_LOGO_URL          = var.brand_logo_url
-    BRAND_FAVICON_URL       = var.brand_favicon_url
+    NODE_ENV              = "production"
+    STRAPI_ADMIN_EMAIL    = var.admin_email
+    STRAPI_ADMIN_PASSWORD = var.admin_password
+    DATABASE_URL          = var.db_connection_string
+    LINKED_STOREFRONT_URL = var.linked_storefront_url
+    BACKEND_URL           = var.backend_url
+    BRAND_PRIMARY_COLOR   = var.brand_primary_color
+    BRAND_SECONDARY_COLOR = var.brand_secondary_color
+    BRAND_LOGO_URL        = var.brand_logo_url
+    BRAND_FAVICON_URL     = var.brand_favicon_url
   }
 }
 
-# Setup GitHub deployment source
+# Configure GitHub Actions deployment for Strapi
 resource "azurerm_app_service_source_control" "github" {
-  app_id             = azurerm_linux_web_app.strapi.id
-  repo_url           = var.repo_url
-  branch             = var.repo_branch
+  app_id                 = azurerm_linux_web_app.strapi.id
+  repo_url               = var.repo_url
+  branch                 = var.repo_branch
   use_manual_integration = true
+
   github_action_configuration {
     code_configuration {
-      code_configuration_source = "GitHub"
-      runtime_stack             = "node"
+      runtime_stack   = "node"
+      runtime_version = "18.x"
     }
   }
-  token = var.github_token
 }
