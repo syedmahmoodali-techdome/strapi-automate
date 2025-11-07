f7c0c24a8b33ee3d20b7638076f864e786bcb5fe
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-07 12:38:06 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,7 +1,6 @@
 # ===========================
 # Azure App Service Plan
 # ===========================
-
 resource "azurerm_service_plan" "strapi" {
   name                = "${var.clinic_name}-plan"
   location            = var.location
@@ -13,7 +12,6 @@ resource "azurerm_service_plan" "strapi" {
 # ===========================
 # Azure Web App
 # ===========================
-
 resource "azurerm_linux_web_app" "strapi" {
   name                = "${var.clinic_name}-cms"
   resource_group_name = var.resource_group_name
@@ -34,38 +32,4 @@ resource "azurerm_linux_web_app" "strapi" {
     BRAND_LOGO_URL        = var.brand_logo_url
     BRAND_FAVICON_URL     = var.brand_favicon_url
   }
-}
-
-# ===========================
-# GitHub Token for Source Control
-# ===========================
-
-resource "azurerm_app_service_source_control_token" "github" {
-  type  = "GitHub"
-  token = var.github_token
-}
-
-# ===========================
-# Connect Web App to GitHub Repo
-# ===========================
-
-resource "azurerm_app_service_source_control" "github" {
-  app_id                 = azurerm_linux_web_app.strapi.id
-  repo_url               = var.repo_url
-  branch                 = var.repo_branch
-  use_manual_integration = false  
-  rollback_enabled       = true
-
-  github_action_configuration {
-    generate_workflow_file = false
-    code_configuration {
-      runtime_stack   = "node"
-      runtime_version = "18.x"
-      code_path       = var.repo_subdir != "" ? var.repo_subdir : "."  # 👈 this is the fix
-    }
-  }
-
-  depends_on = [
-    azurerm_app_service_source_control_token.github
-  ]
 }
