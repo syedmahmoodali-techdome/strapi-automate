24b482a99d30303b7f4b5225e433efe7cd031706
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-07 17:06:13 +0530

    Delete modules/app_service/outputs.tf

--- modules/app_service/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "cms_url" {
-  description = "The default hostname (URL) of the Strapi CMS web app"
-  value       = azurerm_linux_web_app.strapi.default_hostname
-}
-
-output "app_service_name" {
-  value = azurerm_linux_web_app.strapi.name
-}
