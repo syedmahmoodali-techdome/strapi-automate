7c4272444b4415668eccc1d229f0adabd0371d39
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-07 13:17:42 +0530

    Update outputs.tf

--- modules/app_service/outputs.tf
+++ modules/app_service/outputs.tf
@@ -2,3 +2,7 @@ output "cms_url" {
   description = "The default hostname (URL) of the Strapi CMS web app"
   value       = azurerm_linux_web_app.strapi.default_hostname
 }
+
+output "app_service_name" {
+  value = azurerm_linux_web_app.strapi.name
+}
