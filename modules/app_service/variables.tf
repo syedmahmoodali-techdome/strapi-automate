2076f74a02ceee3ae1530f3f1eb9cdd9c4dd1842
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-10 19:05:42 +0530

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,46 +1,19 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "plan_sku" {}
-variable "clinic_name" {}
-variable "repo_branch" {}
-variable "repo_subdir" {}
-variable "admin_email" {}
-variable "admin_password" {
-  sensitive = true
-}
-variable "db_connection_string" {
-  sensitive = true
-}
-variable "linked_storefront_url" {}
-variable "backend_url" {}
-variable "brand_primary_color" {}
-variable "brand_secondary_color" {}
-variable "brand_logo_url" {}
-variable "brand_favicon_url" {}
+variable "service_plan_id" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "clinic_name" { type = string }
+variable "repo_url" { type = string }
+variable "repo_branch" { type = string }
+variable "repo_subdir" { type = string }
+variable "admin_email" { type = string }
+variable "admin_password" { type = string }
+variable "db_connection_string" { type = string }
+variable "plan_sku" { type = string }
+variable "linked_storefront_url" { type = string }
+variable "backend_url" { type = string }
+variable "github_token" { type = string }
 
-variable "github_token" {
-  description = "GitHub Personal Access Token"
-  sensitive   = true
-}
-
-variable "repo_url" {
-  description = "GitHub repository URL for the Strapi source"
-  type        = string
-}
-
-variable "branch" {
-  description = "Branch name for deployment"
-  type        = string
-  default     = "main"
-}
-
-variable "service_plan_id" {
-  description = "The ID of the Azure App Service plan where the app will be deployed"
-  type        = string
-}
-
-variable "azure_app_service_plan_sku" {
-  type        = string
-  description = "SKU name for the Azure App Service Plan, e.g., B1, B2"
-  default     = "B1" # Optional default
-}
+variable "brand_primary_color" { type = string }
+variable "brand_secondary_color" { type = string }
+variable "brand_logo_url" { type = string }
+variable "brand_favicon_url" { type = string }
