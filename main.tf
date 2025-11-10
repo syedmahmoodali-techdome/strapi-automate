954fc9f3752ee7f0957b60ee7f85adbdaa81afef
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-10 10:06:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,4 +52,6 @@ module "app_service" {
   brand_secondary_color = var.brand_secondary_color
   brand_logo_url        = var.brand_logo_url
   brand_favicon_url     = var.brand_favicon_url
+  repo_url = local.config.strapi.repo
+  branch   = local.config.strapi.branch
 }
