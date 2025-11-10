d4c5a17789c4615255071743a462ca6d4310a924
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-10 17:00:56 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,44 +1,57 @@
+# -------------------------------
+# Azure Container Registry (ACR)
+# -------------------------------
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.clinic_name}acr"
+  # Azure ACR names: only lowercase letters + numbers, 5–50 chars
+  name                = "${replace(lower(var.clinic_name), "-", "")}acr"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
+# -------------------------------
+# Azure Linux Web App (App Service)
+# -------------------------------
 resource "azurerm_linux_web_app" "app_service" {
-  name                = "${var.clinic_name}-app"
+  # Web app names can have hyphens, but must be unique
+  name                = "${lower(var.clinic_name)}-app"
   resource_group_name = var.resource_group_name
   location            = var.location
   service_plan_id     = var.service_plan_id
 
   site_config {
-    always_on = true
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/strapi-app:latest"
+    always_on         = true
+    linux_fx_version  = "DOCKER|${azurerm_container_registry.acr.login_server}/strapi-app:latest"
   }
 
   app_settings = {
-    WEBSITES_PORT                     = "1337"
-    DOCKER_REGISTRY_SERVER_URL        = "https://${azurerm_container_registry.acr.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME   = azurerm_container_registry.acr.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD   = azurerm_container_registry.acr.admin_password
+    WEBSITES_PORT                   = "1337"
+    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
   }
 }
 
+# -------------------------------
+# Outputs
+# -------------------------------
 output "acr_name" {
-  value = azurerm_container_registry.acr.name
+  description = "Name of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.name
 }
 
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  description = "Login server URL of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
 }
 
 output "cms_url" {
-  description = "The URL of the deployed Strapi CMS"
+  description = "Public URL of the deployed Strapi CMS"
   value       = azurerm_linux_web_app.app_service.default_hostname
 }
 
 output "app_service_name" {
-  description = "The name of the Azure App Service"
+  description = "Name of the Azure App Service"
   value       = azurerm_linux_web_app.app_service.name
 }
