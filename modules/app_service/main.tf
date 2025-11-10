03b3ac2221f007f04fdd3d8c3a258c558a5aecdf
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-10 17:47:49 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -30,3 +30,15 @@ output "cms_url" {
   value = azurerm_linux_web_app.app_service.default_hostname
 }
     
+output "app_service_name" {
+  description = "Name of the Azure App Service"
+  value       = azurerm_linux_web_app.app_service.name
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
