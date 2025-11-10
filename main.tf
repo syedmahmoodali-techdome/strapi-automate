2e36c0ca10c354d73df4f40ac2891d98f34ef75f
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-10 18:17:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.50.0"
+      version = "~> 3.0"
     }
   }
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.3.0"
 }
-
-
 
 provider "azurerm" {
   features {}
