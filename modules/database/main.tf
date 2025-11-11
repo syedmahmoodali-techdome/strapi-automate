b71a7ddc7be18aab1dedb7153c37632ed2c46bb3
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-11 18:29:45 +0530

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,18 +1,37 @@
-resource "azurerm_postgresql_flexible_server" "this" {
-  name = "${var.db_name}-pg"
-  resource_group_name = var.resource_group_name
-  location = var.location
-  administrator_login = var.db_username
-  administrator_password = var.db_password
-  sku_name = "B_Standard_B1ms"
-  storage_mb = 32768
-  version = "13"
+provider "azurerm" {
+  # inherits provider config
+  features {}
 }
 
-// azurerm_postgresql_flexible_server_database
-// azurerm_postgresql_flexible_database
+resource "azurerm_postgresql_flexible_server" "this" {
+  name                = "${var.db_name}-pg"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  administrator_login = var.db_username
+  administrator_password = var.db_password
+
+  sku_name   = "B_Standard_B1ms"
+  storage_mb = 32768
+  version    = "13"
+
+  # allow public access so App Service (without VNet) can reach it
+  public_network_access_enabled = true
+
+  # recommended defaults
+  backup_retention_days = 7
+  auto_grow_enabled     = true
+}
+
+# Allow Azure services to connect (start/end 0.0.0.0)
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure" {
+  name      = "allow-azure-services"
+  server_id = azurerm_postgresql_flexible_server.this.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
 resource "azurerm_postgresql_flexible_server_database" "db" {
-  name = var.db_name
+  name      = var.db_name
   server_id = azurerm_postgresql_flexible_server.this.id
 }
 
@@ -20,8 +39,12 @@ output "db_fqdn" {
   value = azurerm_postgresql_flexible_server.this.fqdn
 }
 
+output "db_port" {
+  value = 5432
+}
+
 output "connection_string" {
-  value = "postgresql://${var.db_username}:${var.db_password}@${azurerm_postgresql_flexible_server.this.fqdn}:5432/${var.db_name}"
+  value     = "postgresql://${var.db_username}:${var.db_password}@${azurerm_postgresql_flexible_server.this.fqdn}:5432/${var.db_name}"
   sensitive = true
 }
 
