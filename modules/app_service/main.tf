95d6aeef9c46ffdc7cf52d144da7043c223b6f74
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-11 18:31:03 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,10 +1,8 @@
-########################
-# App Service Module
-########################
+provider "azurerm" {
+  features {}
+}
 
-# ----------------------
-# Container Registry
-# ----------------------
+# Container Registry (ACR)
 resource "azurerm_container_registry" "acr" {
   name                = "${replace(lower(var.clinic_name), "-", "")}acr"
   resource_group_name = var.resource_group_name
@@ -13,9 +11,7 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-# ----------------------
 # Linux Web App
-# ----------------------
 resource "azurerm_linux_web_app" "app_service" {
   name                = "${lower(var.clinic_name)}-app"
   resource_group_name = var.resource_group_name
@@ -24,39 +20,43 @@ resource "azurerm_linux_web_app" "app_service" {
 
   site_config {
     always_on = true
+    # NOTE: we do NOT set linux_fx_version or container_registry here so pipeline (az cli) can manage the image
   }
 
   app_settings = {
-    WEBSITES_PORT        = "1337"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    WEBSITES_PORT                        = "1337"
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE  = "false"
 
-    # DB Variables (Dynamic)
-    DATABASE_CLIENT   = "postgres"
-    DATABASE_HOST     = var.db_host
-    DATABASE_PORT     = tostring(var.db_port)
-    DATABASE_NAME     = var.db_name
-    DATABASE_USERNAME = var.db_user
-    DATABASE_PASSWORD = var.db_password
+    # DB
+    DATABASE_CLIENT      = "postgres"
+    DATABASE_HOST        = var.db_host
+    DATABASE_PORT        = tostring(var.db_port)
+    DATABASE_NAME        = var.db_name
+    DATABASE_USERNAME    = var.db_user
+    DATABASE_PASSWORD    = var.db_password
 
-    # Branding (optional)
+    # Strapi admin
+    STRAPI_ADMIN_EMAIL    = var.strapi_admin_email
+    STRAPI_ADMIN_PASSWORD = var.strapi_admin_password
+
+    # Strapi secrets
+    APP_KEYS             = var.app_keys
+    API_TOKEN_SALT       = var.api_token_salt
+    ADMIN_JWT_SECRET     = var.admin_jwt_secret
+    TRANSFER_TOKEN_SALT  = var.transfer_token_salt
+
+    # Optional integrations & branding
+    LINKED_STOREFRONT_URL = var.linked_storefront_url
+    BACKEND_URL           = var.backend_url
+
     BRAND_PRIMARY_COLOR   = var.brand_primary_color
     BRAND_SECONDARY_COLOR = var.brand_secondary_color
     BRAND_LOGO_URL        = var.brand_logo_url
     BRAND_FAVICON_URL     = var.brand_favicon_url
-
-    # Strapi Admin
-    STRAPI_ADMIN_EMAIL    = var.strapi_admin_email
-    STRAPI_ADMIN_PASSWORD = var.strapi_admin_password
-
-    # Linked integrations
-    LINKED_STOREFRONT_URL = var.linked_storefront_url
-    BACKEND_URL           = var.backend_url
   }
 }
 
-# ----------------------
 # Outputs
-# ----------------------
 output "app_service_name" {
   value = azurerm_linux_web_app.app_service.name
 }
@@ -71,4 +71,15 @@ output "acr_name" {
 
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
+}
+
+# Provide ACR admin credentials for pipeline convenience
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
