06b4a3fdb2e29bdcb30121df111bc60f4cd0ca62
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-11 18:27:39 +0530

    Update main.tf

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,18 +1,3 @@
-variable "name_prefix" {
-  type        = string
-  description = "prefix to use in resource group name"
-}
-
-variable "environment" {
-  type        = string
-  description = "environment name (staging/prod)"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region"
-}
-
 resource "azurerm_resource_group" "this" {
   name     = "${var.name_prefix}-${var.environment}-strapi-cms-rg"
   location = var.location
