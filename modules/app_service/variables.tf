392a0df059631483e7eeb2fd4e3f1b59690558ab
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-11 18:30:47 +0530

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,167 +1,133 @@
-###########################################
-# app_service Module Variables
-###########################################
-
 # Basic info
 variable "clinic_name" {
-  description = "Clinic name"
   type        = string
+  description = "Clinic name"
 }
 
 variable "resource_group_name" {
-  description = "Resource group name"
   type        = string
+  description = "Resource group name"
 }
 
 variable "location" {
-  description = "Azure region"
   type        = string
+  description = "Azure region"
 }
 
 variable "service_plan_id" {
-  description = "App Service Plan ID"
   type        = string
+  description = "App Service Plan ID"
 }
 
-# Container image
+# Container image placeholders (pipeline will set actual image)
 variable "image_name" {
-  description = "Docker image name for Strapi"
   type        = string
+  description = "Docker image name (placeholder)"
+  default     = "strapi-cms"
 }
 
 variable "image_tag" {
-  description = "Docker image tag for Strapi"
   type        = string
+  description = "Docker image tag (placeholder)"
+  default     = "latest"
 }
 
-# Database configuration
+# Database config
 variable "db_host" {
-  description = "Database host"
   type        = string
+  description = "Database host / FQDN"
 }
 
 variable "db_port" {
-  description = "Database port"
   type        = number
+  description = "Database port"
   default     = 5432
 }
 
 variable "db_name" {
-  description = "Database name"
   type        = string
+  description = "Database name"
 }
 
 variable "db_user" {
-  description = "Database username"
   type        = string
+  description = "Database username"
 }
 
 variable "db_password" {
-  description = "Database password"
   type        = string
+  description = "Database password"
   sensitive   = true
 }
 
 # Strapi admin
 variable "strapi_admin_email" {
-  description = "Admin email for Strapi CMS"
   type        = string
 }
 
 variable "strapi_admin_password" {
-  description = "Admin password for Strapi CMS"
   type        = string
   sensitive   = true
 }
 
-# Secrets
+# Secrets (passed from root randoms)
 variable "app_keys" {
-  description = "Strapi APP_KEYS"
   type        = string
   sensitive   = true
 }
 
 variable "api_token_salt" {
-  description = "Strapi API_TOKEN_SALT"
   type        = string
   sensitive   = true
 }
 
 variable "admin_jwt_secret" {
-  description = "Strapi ADMIN_JWT_SECRET"
   type        = string
   sensitive   = true
 }
 
 variable "transfer_token_salt" {
-  description = "Strapi TRANSFER_TOKEN_SALT"
   type        = string
   sensitive   = true
 }
 
-# Optional integrations
+# Optional fields
 variable "linked_storefront_url" {
-  description = "Linked storefront URL"
-  type        = string
-  default     = ""
+  type    = string
+  default = ""
 }
 
 variable "backend_url" {
-  description = "Backend URL"
-  type        = string
-  default     = ""
+  type    = string
+  default = ""
 }
 
 variable "github_token" {
-  description = "GitHub PAT for deployment"
-  type        = string
-  sensitive   = true
-  default     = ""
+  type      = string
+  sensitive = true
+  default   = ""
 }
 
 # Branding
 variable "brand_primary_color" {
-  type        = string
-  description = "Primary brand color"
-  default     = ""
+  type    = string
+  default = ""
 }
-
 variable "brand_secondary_color" {
-  type        = string
-  description = "Secondary brand color"
-  default     = ""
+  type    = string
+  default = ""
 }
-
 variable "brand_logo_url" {
-  type        = string
-  description = "Logo URL"
-  default     = ""
+  type    = string
+  default = ""
 }
-
 variable "brand_favicon_url" {
-  type        = string
-  description = "Favicon URL"
-  default     = ""
+  type    = string
+  default = ""
 }
 
 # Metadata
-variable "plan_sku" {
-  description = "Azure App Service Plan SKU"
-  type        = string
-}
-
-variable "repo_url" {
-  description = "Strapi repository URL"
-  type        = string
-}
-
-variable "repo_branch" {
-  description = "Branch to deploy from"
-  type        = string
-}
-
-variable "repo_subdir" {
-  description = "Subdirectory within repo"
-  type        = string
-  default     = ""
-}
+variable "plan_sku" { type = string }
+variable "repo_url" { type = string }
+variable "repo_branch" { type = string }
+variable "repo_subdir" { type = string, default = "" }
