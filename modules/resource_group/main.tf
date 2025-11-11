9f5925421015012413c93b34a25015050428df2b
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-11 18:26:05 +0530

    Update main.tf

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,16 +1,27 @@
+variable "name_prefix" {
+  type        = string
+  description = "prefix to use in resource group name"
+}
+
+variable "environment" {
+  type        = string
+  description = "environment name (staging/prod)"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
 resource "azurerm_resource_group" "this" {
-  name = "${var.name_prefix}-${var.environment}-strapi-cms-rg" 
+  name     = "${var.name_prefix}-${var.environment}-strapi-cms-rg"
   location = var.location
 }
 
 output "name" {
-  value = azurerm_resource_group.this.name  
+  value = azurerm_resource_group.this.name
 }
 
 output "resource_group_name" {
   value = azurerm_resource_group.this.name
-}
-
-output "location" {
-  value = azurerm_resource_group.this.location
 }
